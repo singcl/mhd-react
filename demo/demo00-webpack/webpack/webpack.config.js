f863077c1dc4bd42cd70baa1b3d244e79a6fc576
singcl <24661881@qq.com>
2017-12-28 02:10:18 +0800

    eslint配置；webpack MD文档丰富

---
+++ demo/demo00-webpack/webpack/webpack.config.js
@@ -0,0 +1,9 @@
+const webpack = require('webpack')
+
+module.exports = {
+  devtool: 'eval-source-map',
+  // externals: {
+  //    'react': 'window.React',
+  //    'react-dom': 'window.ReactDOM'
+  // },
+}
