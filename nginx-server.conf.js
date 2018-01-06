4e121dd61c8b026686489a3962df1f47f5d00e9c
singcl <24661881@qq.com>
2018-01-06 21:42:28 +0800

    重写nginx-server.conf.js 逻辑更加简洁清晰 + 忽略不必要上传的文件

--- nginx-server.conf.js
+++ nginx-server.conf.js
@@ -1,5 +1,7 @@
+/* eslint-disable no-console */
 var fs = require('fs')
 var path = require('path')
+
 /* ------------------------------------- nginx server 配置处 ------------------------------------- */
 /*栗子*/
 var data = [
@@ -19,13 +21,6 @@ var data = [
     }
 ]
 
-/*备注*/
-
-// nginx -s stop 关闭服务
-// ajax请求的时候，用相对路径
-// 开启nginx服务需要到文件夹中双击打开，切莫IDE里打开
-
-/* ------------------------------------- 以下为处理事件 ------------------------------------- */
 // 反向代理服务器配置模板
 var serverTpl = (data) => {
 return {
@@ -52,86 +47,29 @@ server {
         root  ${data.path};
     }
 }`
-// 字符串拼接模板
-/*
-'#' +
-data.name +
-'\n' +
-'server{\n' +
-'listen  ' +
-data.port +
-';\n' +
-'server_name 127.0.0.1 localhost;\n' +
-'access_log  logs/' +
-data.name +
-'.log;\n' +
-'charset utf-8;\n' +
-'location =/ {\n' +
-'rewrite / /' +
-data.index +
-'.html permanent;\n' +
-'}\n' +
-'location / {\n' +
-'proxy_pass  ' +
-data.url +
-';\n' +
-'proxy_redirect  default;\n' +
-'}\n' +
-'location ~* ^.+.(ico|gif|jpg|jpeg|png|html)$ {\n' +
-'root  ' +
-data.path +
-';\n' +
-'}\n' +
-'location ~* ^.+.(css|js|txt|xml|swf|wav|json)$ {\n' +
-'root  ' +
-data.path +
-';\n' +
-'}\n' +
-'}\n'
-*/
 }}
 
-/* eslint-disable no-console */
-var _nodeInit = function(data) {
-    /*初始化*/
-    var sum = 0
-    var pathImg = 'nginx/conf/vhost/'
-    files = fs.readdirSync(pathImg)
-    files.forEach(function(file, index) {
-        // 记总共步数
-        sum = index
-    })
-    if (fs.existsSync(pathImg)) {
-        // 先清空内部所有配置文件
-        files.forEach(function(file, index) {
-            var curPath = pathImg + '/' + file
-            if (fs.statSync(curPath).isDirectory()) {
-                fs.rmdirSync(pathImg)
-            } else {
-                fs.unlinkSync(curPath, function(err) {
-                    if (err) throw err
-                })
-            }
-            if (index === sum) {
-                // 结束时添加
-                for (var i = 0; i < data.length; i++) {
-                    (function(data) {
-                        /*conf/vhost/为文件默认创建地方*/
-                        fs.appendFile(
-                            pathImg + data.name + '.conf',
-                            serverTpl(data).val,
-                            'utf8',
-                            function(err) {
-                                if (err) {
-                                    return console.error(err)
-                                }
-                            }
-                        )
-                    })(data[i])
+/*备注*/
+
+// nginx -s stop 关闭服务
+// ajax请求的时候，用相对路径
+
+/* ------------------------------------- 以下为写入NGXIN 配置 ------------------------------------- */
+// 写反向代理服务器配置文件，不存在则创建 http://nodejs.cn/api/fs.html#fs_fs_access_path_mode_callback
+function proxyServerConf(data) {
+    data.forEach(function(item, index) {
+        var filePath = path.resolve(__dirname, `./nginx/conf/vhost/${item.name}.conf`)
+        fs.writeFile(
+            filePath,
+            serverTpl(item).val,
+            'utf8',
+            function(err) {
+                if (err) {
+                    return console.error(err)
                 }
             }
-        })
-    }
+        )
+    })
 }
 
-_nodeInit(data)
+proxyServerConf(data)
