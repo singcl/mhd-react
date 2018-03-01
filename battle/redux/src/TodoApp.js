96a7b3e908352ebc49c2fabbe83fc21e37e2727a
singcl <24661881@qq.com>
2018-03-01 19:06:22 +0800

    :rotating_light: eslint文件更新

--- battle/redux/src/TodoApp.js
+++ battle/redux/src/TodoApp.js
@@ -1,3 +1,5 @@
+// https://github.com/yannickcr/eslint-plugin-react/blob/master/docs/rules/react-in-jsx-scope.md
+// 要么把React import进来，要么把react/react-in-jsx-scope这个规则disable掉
 import React from 'react'
 import { view as Todos } from './todos/'
 import { view as Filter } from './filter/'
