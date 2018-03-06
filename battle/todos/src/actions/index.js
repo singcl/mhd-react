992caac72b308f1c08832f458c39c7525afe9f24
singcl <24661881@qq.com>
2018-03-07 00:47:21 +0800

    :art: filter link 构建中...

--- battle/todos/src/actions/index.js
+++ battle/todos/src/actions/index.js
@@ -11,3 +11,9 @@ export const toggleTodo = id => ({
     type: 'TOGGLE_TODO',
     id
 })
+
+export const visibilityFilters = {
+    SHOW_ALL: 'SHOW_ALL',
+    SHOW_COMPLETED: 'SHOW_COMPLETED',
+    SHOW_ACTIVE: 'SHOW_ACTIVE'
+}
