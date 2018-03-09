5d9fe3cb40018e3ae1ab6ae02d1bfb7ee3bdd7b0
singcl <24661881@qq.com>
2018-03-09 11:06:27 +0800

    :zap: :art: pkg更新

--- battle/shopping-cart/src/components/Product.js
+++ battle/shopping-cart/src/components/Product.js
@@ -1,17 +1,17 @@
 import React from 'react'
-import PropTyles from 'prop-types'
+import PropTypes from 'prop-types'
 
 const Product = ({ title, price, quantity }) => (
     <div>
         {title} - &#36; {price}
-        {quantity ? `x${quantity}` : null}
+        {quantity ? ` x ${quantity}` : null}
     </div>
 )
 
 Product.propTypes = {
-    title: PropTyles.string.isRequired,
-    price: PropTyles.number.isRequired,
-    quantity: PropTyles.number.isRequired
+    title: PropTypes.string.isRequired,
+    price: PropTypes.number.isRequired,
+    quantity: PropTypes.number.isRequired
 }
 
 export default Product
