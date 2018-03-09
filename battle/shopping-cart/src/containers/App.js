c8fddeea69a98cf2e2d5e0df09c4818cd185308c
singcl <24661881@qq.com>
2018-03-09 13:16:25 +0800

    :construction: CART CONTAINER 构建中...

--- battle/shopping-cart/src/containers/App.js
+++ battle/shopping-cart/src/containers/App.js
@@ -1,10 +1,14 @@
 import React from 'react'
 import ProductsContainer from './ProductsContainer'
+import CartContainer from './CartContainer'
 
 const App = () => (
     <div>
         <h2>SHOP</h2>
+        <hr />
         <ProductsContainer />
+        <hr />
+        <CartContainer />
     </div>
 )
 
---
+++ battle/shopping-cart/src/containers/CartContainer.js
@@ -0,0 +1,5 @@
+import React from 'react'
+
+const CartContainer = () => <div>CartContainer</div>
+
+export default CartContainer
