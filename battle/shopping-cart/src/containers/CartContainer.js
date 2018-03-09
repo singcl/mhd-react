f6b8d0783ff70cda2776b98580af12ccf18c9c8b
singcl <24661881@qq.com>
2018-03-09 21:21:27 +0800

    :sparkles:  mapDispatchToProps 两种表示法中的函数表示法

--- battle/shopping-cart/src/containers/CartContainer.js
+++ battle/shopping-cart/src/containers/CartContainer.js
@@ -4,6 +4,7 @@ import { connect } from 'react-redux'
 import { getCartProducts, getTotal } from '../reducers'
 import { checkout } from '../actions'
 import Cart from '../components/Cart'
+import { bindActionCreators } from 'redux'
 
 const CartContainer = ({ products, total, checkout }) => (
     <Cart
@@ -31,4 +32,12 @@ const mapStateToProps = state => ({
     total: getTotal(state)
 })
 
-export default connect(mapStateToProps, { checkout })(CartContainer)
+export default connect(
+    mapStateToProps,
+    // 方法一: mapDispatchToProps 两种表示法中的函数表示法
+    dispatch => ({
+        ...bindActionCreators({ checkout }, dispatch)
+    })
+    // 方法二: mapDispatchToProps 两种表示法中的对象表示法 // react-redux 内部会包装成函数的写法
+    // { checkout }
+)(CartContainer)
