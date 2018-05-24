8b05afdb60362de8cd715eff3af5f82eeb4e7837
singcl <24661881@qq.com>
2018-05-24 17:19:52 +0800

    fix

--- battle/async/src/containers/App.js
+++ battle/async/src/containers/App.js
@@ -1,26 +1,26 @@
-import PropTypes from "prop-types";
-import React from "react";
-import { connect } from "react-redux";
-import { fetchPostsIfNeeded } from "../actions";
+import PropTypes from 'prop-types'
+import React from 'react'
+import { connect } from 'react-redux'
+import { fetchPostsIfNeeded } from '../actions'
 
 class App extends React.Component {
     componentDidMount() {
-        const { dispatch, selectedSubreddit } = this.props;
-        dispatch(fetchPostsIfNeeded(selectedSubreddit));
+        const { dispatch, selectedSubreddit } = this.props
+        dispatch(fetchPostsIfNeeded(selectedSubreddit))
     }
 
     render() {
-        return <div>Async App</div>;
+        return <div>Async App</div>
     }
 }
 
 App.propTypes = {
     dispatch: PropTypes.func.isRequired,
     selectedSubreddit: PropTypes.string.isRequired
-};
+}
 
-const mapStateToProps = state => ({
+const mapStateToProps = (state) => ({
     selectedSubreddit: state.selectedSubreddit
-});
+})
 
-export default connect(mapStateToProps)(App);
+export default connect(mapStateToProps)(App)
