150880bcd89ae2c8b2ada9466b52ec5c7daca5cf
singcl <24661881@qq.com>
2018-05-24 14:55:26 +0800

    :art: 使用eslint 的规则进行prettier

--- battle/async/src/index.js
+++ battle/async/src/index.js
@@ -1,11 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
 import { Provider } from 'react-redux'
+import { applyMiddleware, createStore } from 'redux'
 import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
-import reducer from './reducers'
 import App from './containers/App'
+import reducer from './reducers'
 import registerServiceWorker from './registerServiceWorker'
 
 /*
