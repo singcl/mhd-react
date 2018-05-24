31aab1ef27c5ad5fa3d14ac78b0b4935219190fe
singcl <24661881@qq.com>
2018-05-24 18:20:49 +0800

    :art: update Picker.js

--- battle/async/src/containers/App.js
+++ battle/async/src/containers/App.js
@@ -1,7 +1,8 @@
 import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
-import { fetchPostsIfNeeded } from '../actions'
+import { fetchPostsIfNeeded, selectSubreddit } from '../actions'
+import Picker from '../components/Picker'
 
 class App extends React.Component {
     componentDidMount() {
@@ -9,18 +10,74 @@ class App extends React.Component {
         dispatch(fetchPostsIfNeeded(selectedSubreddit))
     }
 
+    handleChange = nextSubreddit => {
+        this.props.dispatch(selectSubreddit(nextSubreddit))
+    }
+
     render() {
-        return <div>Async App</div>
+        const { selectedSubreddit, posts, isFetching, lastUpdated } = this.props
+        const isEmpty = posts.length === 0
+
+        return (
+            <div>
+                <Picker
+                    value={selectedSubreddit}
+                    onChange={this.handleChange}
+                    options={['reactjs', 'frontend']}
+                />
+
+                <p>
+                    {lastUpdated && (
+                        <span>
+                            Last updated at{' '}
+                            {new Date(lastUpdated).toLocaleTimeString()}.{' '}
+                        </span>
+                    )}
+
+                    {!isFetching && (
+                        <button onClick={this.handleChangeRefreshClick}>
+                            Refresh
+                        </button>
+                    )}
+                </p>
+
+                {isEmpty ? (
+                    isFetching ? (
+                        <h2>Loading...</h2>
+                    ) : (
+                        <h2>Empty.</h2>
+                    )
+                ) : (
+                    <div style={{ opacity: isFetching ? 0.5 : 1 }}>有数据</div>
+                )}
+            </div>
+        )
     }
 }
 
 App.propTypes = {
     dispatch: PropTypes.func.isRequired,
-    selectedSubreddit: PropTypes.string.isRequired
+    selectedSubreddit: PropTypes.string.isRequired,
+    posts: PropTypes.array.isRequired,
+    isFetching: PropTypes.bool.isRequired,
+    lastUpdated: PropTypes.number
 }
 
-const mapStateToProps = (state) => ({
-    selectedSubreddit: state.selectedSubreddit
-})
+const mapStateToProps = state => {
+    const { selectedSubreddit, postsBySubreddit } = state
+    const { isFetching, lastUpdated, items: posts } = postsBySubreddit[
+        selectedSubreddit
+    ] || {
+        isFetching: true,
+        items: []
+    }
+
+    return {
+        selectedSubreddit,
+        posts,
+        isFetching,
+        lastUpdated
+    }
+}
 
 export default connect(mapStateToProps)(App)
