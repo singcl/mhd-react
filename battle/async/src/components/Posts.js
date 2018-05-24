1290bb76d73cee2052d1e016e47088c1bd528ed5
singcl <24661881@qq.com>
2018-05-24 18:56:27 +0800

    :art: use a list

--- battle/async/src/components/Posts.js
+++ battle/async/src/components/Posts.js
@@ -2,7 +2,13 @@ import PropTypes from 'prop-types'
 import React from 'react'
 
 const Posts = ({ posts }) => (
-    <ul>{posts.map((post, i) => <li key={i}>{post.title}</li>)}</ul>
+    <ul>
+        {posts.map((post, i) => (
+            <li key={i}>
+                <a href={post.url}>{post.title}</a>
+            </li>
+        ))}
+    </ul>
 )
 
 Posts.propTypes = {
