17921d89282d534e0e1096c8b2f4c89b93df8631
singcl <24661881@qq.com>
2018-05-25 10:28:46 +0800

    :art: create todos-with-undo App

--- battle/shopping-cart/src/actions/index.js
+++ battle/shopping-cart/src/actions/index.js
@@ -1,5 +1,5 @@
-import shop from '../api/shop'
-import * as types from '../constants/ActionTypes'
+import shop from '../api/shop';
+import * as types from '../constants/ActionTypes';
 
 const receiveProducts = products => ({
     type: types.RECEIVE_PRODUCTS,
