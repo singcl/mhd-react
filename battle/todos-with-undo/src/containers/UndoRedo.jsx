0427143f3480126b2aaf62aa9f284498450efeea
singcl <24661881@qq.com>
2018-05-25 17:59:03 +0800

    :art: Redux-undo is nb

--- battle/todos-with-undo/src/containers/UndoRedo.jsx
+++ battle/todos-with-undo/src/containers/UndoRedo.jsx
@@ -1,5 +1,34 @@
+import PropTypes from 'prop-types'
 import React from 'react'
+import { connect } from 'react-redux'
+import { ActionCreators as UndoActionCreators } from 'redux-undo'
 
-const UndoRedo = () => <div>UndoRedo</div>
+const UndoRedo = ({ canUndo, canRedo, onUndo, onRedo }) => (
+    <p>
+        <button onClick={onUndo} disabled={!canUndo}>
+            Undo
+        </button>
+        <button onClick={onRedo} disabled={!canRedo}>
+            Redo
+        </button>
+    </p>
+)
 
-export default UndoRedo
+UndoRedo.propTypes = {
+    canRedo: PropTypes.bool.isRequired,
+    canUndo: PropTypes.bool.isRequired,
+    onRedo: PropTypes.func.isRequired,
+    onUndo: PropTypes.func.isRequired
+}
+
+const mapStateToProps = (state) => ({
+    canUndo: state.todos.past.length > 0,
+    canRedo: state.todos.future.length > 0
+})
+
+const mapDisPatchToProps = {
+    onUndo: UndoActionCreators.undo,
+    onRedo: UndoActionCreators.redo
+}
+
+export default connect(mapStateToProps, mapDisPatchToProps)(UndoRedo)
