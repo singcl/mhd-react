c9cac740b7b38298d1fee9e066c03a096542581f
singcl <24661881@qq.com>
2018-05-30 10:13:27 +0800

    :art: App component

--- battle/real-world/src/actions/index.js
+++ battle/real-world/src/actions/index.js
@@ -1,12 +1,6 @@
 import * as types from '../constants/ActionTypes'
 
-export const addTodo = (text) => ({ type: types.ADD_TODO, text })
-export const deleteTodo = (id) => ({ type: types.DELETE_TODO, id })
-export const editTodo = (id, text) => ({ type: types.EDIT_TODO, id, text })
-export const completeTodo = (id) => ({ type: types.COMPLETE_TODO, id })
-export const completeAllTodos = () => ({ type: types.COMPLETE_ALL_TODOS })
-export const clearCompleted = () => ({ type: types.CLEAR_COMPLETED })
-export const setVisibilityFilter = (filter) => ({
-    type: types.SET_VISIBILITY_FILTER,
-    filter
+// Resets the currently visible error message.
+export const resetErrorMessage = () => ({
+    type: types.RESET_ERROR_MESSAGE
 })
--- battle/real-world/src/containers/App.jsx
+++ battle/real-world/src/containers/App.jsx
@@ -1,19 +1,53 @@
+/* eslint-disable react/sort-prop-types */
 import PropTypes from 'prop-types'
 import React from 'react'
 import { connect } from 'react-redux'
 import { withRouter } from 'react-router-dom'
+import { resetErrorMessage } from '../actions'
 import Explore from '../components/Explore.jsx'
 
 class App extends React.Component {
     static propTypes = {
+        // Injected by React Router
         children: PropTypes.node,
-        inputValue: PropTypes.string.isRequired
+        history: PropTypes.object.isRequired,
+        // Injected by React Redux
+        errorMessage: PropTypes.string,
+        inputValue: PropTypes.string.isRequired,
+        resetErrorMessage: PropTypes.func.isRequired
     }
+
+    handleChange = (nextValue) => {
+        const { history } = this.props
+        history.push(`/${nextValue}`)
+    }
+
+    handleDismissClick = (e) => {
+        this.props.resetErrorMessage()
+        e.preventDefault()
+    }
+
+    renderErrorMessage() {
+        const { errorMessage } = this.props
+        if (!errorMessage) {
+            return null
+        }
+
+        return (
+            <p style={{ backgroundColor: '#e99', padding: 10 }}>
+                <b>{errorMessage}</b>{' '}
+                <button onClick={this.handleDismissClick}>Dismiss</button>
+            </p>
+        )
+    }
+
     render() {
         const { children, inputValue } = this.props
         return (
             <div>
-                <Explore />
+                <Explore value={inputValue} onChange={this.handleChange} />
+                <hr />
+                {this.renderErrorMessage()}
                 {children}
             </div>
         )
@@ -21,7 +55,15 @@ class App extends React.Component {
 }
 
 const mapStateToProps = (state, ownProps) => ({
+    errorMessage: state.errorMessage,
     inputValue: ownProps.location.pathname.substring(1)
 })
 
-export default withRouter(connect(mapStateToProps)(App))
+const mapDispatchToProps = { resetErrorMessage }
+
+export default withRouter(
+    connect(
+        mapStateToProps,
+        mapDispatchToProps
+    )(App)
+)
