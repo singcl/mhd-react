221720ae04e7931b1edd3f40f1a86bd3f178175f
singcl <24661881@qq.com>
2018-05-31 15:48:33 +0800

    :art: update real-world build file jsx

--- battle/real-world/src/containers/Root.prod.jsx
+++ battle/real-world/src/containers/Root.prod.jsx
@@ -1,5 +1,23 @@
+import PropTypes from 'prop-types'
 import React from 'react'
+import { Provider } from 'react-redux'
+import { Route } from 'react-router-dom'
+import App from './App.jsx'
+import RepoPage from './RepoPage.jsx'
+import UserPage from './UserPage.jsx'
 
-const Root = () => <div>ROOT_PROD</div>
+const Root = ({ store }) => (
+    <Provider store={store}>
+        <div>
+            <Route path="/" component={App} />
+            <Route path="/:login/:name" component={RepoPage} />
+            <Route path="/:login" component={UserPage} />
+        </div>
+    </Provider>
+)
+
+Root.propTypes = {
+    store: PropTypes.object.isRequired
+}
 
 export default Root
--- battle/real-world/src/store/configureStore.prod.js
+++ battle/real-world/src/store/configureStore.prod.js
@@ -1,8 +1,14 @@
 import { applyMiddleware, createStore } from 'redux'
+// import { createLogger } from 'redux-logger'
 import thunk from 'redux-thunk'
+import api from '../middleware/api'
 import rootReducer from '../reducers'
 
 const configureStore = (preloadedState) =>
-    createStore(rootReducer, preloadedState, applyMiddleware(thunk))
+    createStore(
+        rootReducer,
+        preloadedState,
+        applyMiddleware(thunk, api /* createLogger() */)
+    )
 
 export default configureStore
