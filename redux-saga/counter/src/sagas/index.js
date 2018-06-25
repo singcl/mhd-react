d3a2637e63a41cdddadc573aa97950db537e8ac6
singcl <24661881@qq.com>
2018-06-25 20:15:42 +0800

    :art: 格式化部分代码

--- redux-saga/counter/src/sagas/index.js
+++ redux-saga/counter/src/sagas/index.js
@@ -1,12 +1,12 @@
 /* eslint-disable no-constant-condition */
 
-import { delay, put, takeEvery } from 'redux-saga/effects';
+import { delay, put, takeEvery } from 'redux-saga/effects'
 
 export function* incrementAsync() {
-  yield delay(1000)
-  yield put({ type: 'INCREMENT' })
+    yield delay(1000)
+    yield put({ type: 'INCREMENT' })
 }
 
 export default function* rootSaga() {
-  yield takeEvery('INCREMENT_ASYNC', incrementAsync)
+    yield takeEvery('INCREMENT_ASYNC', incrementAsync)
 }
